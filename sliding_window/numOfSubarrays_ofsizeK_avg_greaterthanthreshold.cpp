f22d3a6dc110890cee7f4e0d7690c70ea4c51336
Pratik1503 <90402439+Pratik1503@users.noreply.github.com>
2023-12-22 22:49:49 +0530

    day-3

---
+++ sliding_window/numOfSubarrays_ofsizeK_avg_greaterthanthreshold.cpp
@@ -0,0 +1,24 @@
+  #include <bits/stdc++.h>
+   using namespace std;
+
+   int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+
+        int i=0,j=0,ans=0,sum=0;
+
+        for(;j<arr.size();j++){
+            sum+=arr[j];
+            if(j-i+1>=k){
+                if(sum/k>=threshold){
+                    ans++;
+                }
+                sum-=arr[i++];
+            }
+        }
+        return ans;
+        
+    }
+
+    int main(){
+        vector<int> v ={2,2,2,2,5,5,5,8};
+        cout<<numOfSubarrays(v, 3, 4);
+    }
